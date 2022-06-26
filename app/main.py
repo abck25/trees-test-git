4258bdad2111eda73422aabb6c9cc1ab50f5937e
Abi <k.abiraam@gmail.com>
2022-06-26 18:58:52 +0200

    deleted unneccsary stuff

--- app/main.py
+++ app/main.py
@@ -1,8 +1,41 @@
 from flask import Flask
+from flask import jsonify, Response
 
-from model import forecaster, pd, db
+# import pandas as pd
+# import numpy as np
+from model import pd
 
+# create the Flask app
 app = Flask(__name__)
+
+
+@app.route('/')
+def toptentreestempelhof():
+
+    # Missing values
+    na_vals = ["-"]
+    # These commands load the trees datasets
+    df_tempelhofGG40 = pd.read_excel(
+        "app/datasets/tempelhofGG40.xls", na_values=na_vals)
+    df_tempelhofK40 = pd.read_excel(
+        "app/datasets/tempelhofK40.xls", na_values=na_vals)
+    df_tempelhof = pd.concat(
+        [df_tempelhofGG40, df_tempelhofK40], ignore_index=True)
+
+    # Show me top 10 types of trees in district (abs.) VERSION 1
+    toptentrees = df_tempelhof["Baumart deutsch"].value_counts().nlargest(
+        n=10).to_json()
+    print(toptentrees)
+    return toptentrees
+
+    # # Show me top 10 types of trees in district (abs.) VERSION 2
+    # toptentrees = df_tempelhof["Baumart deutsch"].value_counts().nlargest(
+    #     n=10)
+    # return Response(toptentrees.to_json(), mimetype='application/json')
+
+    # # quick look about the information of the dataframe
+    # exampleten = df_tempelhof.head(10).to_json()
+    # return exampleten
 
 
 # def predict_static():
@@ -24,26 +57,21 @@ app = Flask(__name__)
 #     return f"<h3>Hello, NYC is {screwed_perc}% screwed today!</h3><br/>{actives} are on the job."
 
 
-@app.route('/')
-def predict_backend():
-    today = pd.Timestamp.now().date()
-    pred = forecaster[forecaster.date < str(today)].tail(7).crisis.median()
-    print(pred)
+# def predict_backend():
+#     today = pd.Timestamp.now().date()
+#     pred = forecaster[forecaster.date < str(today)].tail(7).crisis.median()
+#     print(pred)
 
-    # fetch results/timesheets from back-end
-    roster = pd.DataFrame([
-        doc.to_dict() for doc in db.collection(u"superhero_timesheets").where(u"date", u">=", str(today)).stream()
-    ])
+#     # fetch results/timesheets from back-end
+#     roster = pd.DataFrame([
+#         doc.to_dict() for doc in db.collection(u"superhero_timesheets").where(u"date", u">=", str(today)).stream()
+#     ])
 
-
-    actives = ", ".join(roster[roster.active_on_duty == 1].superhero.unique().tolist())
-    n_result = roster.active_on_duty.sum()
-    print(today)
-    print(roster)
-
-    screwed_perc =  float(pred - n_result)/pred * 100 if pred > n_result else 0
-    return f"<h3>Hello, NYC is {screwed_perc}% screwed today!</h3><br/>{actives} are on the job."
-
-
+#     actives = ", ".join(roster[roster.active_on_duty == 1].superhero.unique().tolist())
+#     n_result = roster.active_on_duty.sum()
+#     print(today)
+#     print(roster)
+#     screwed_perc =  float(pred - n_result)/pred * 100 if pred > n_result else 0
+#     return f"<h3>Hello, NYC is {screwed_perc}% screwed today!</h3><br/>{actives} are on the job."
 if __name__ == "__main__":
     app.run()
